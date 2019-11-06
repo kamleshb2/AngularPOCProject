d3a6e2497fb784e93193132edacbd2a053d1c224
Kamlesh Badgujar <KamleshB2@citiustech.com>
2019-11-06 15:15:07 +0530

    added mentions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,11 @@ import { AppRoutingModule } from './app-routing.module';
 import {NgMentionsModule} from '@nth-cloud/ng-mentions';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgMentionModule } from 'angular-mention';
+import {NgxTributeModule} from 'ngx-tribute';
+
 
 @NgModule({
   declarations: [
@@ -17,8 +20,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    NgxTributeModule, 
     AppRoutingModule,
     TableModule,
+    NgMentionModule,
     DropdownModule,
     DialogModule,
     FormsModule,
